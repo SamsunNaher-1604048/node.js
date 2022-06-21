362b8ae0b6ff07e0586ba3ad1781a89ca8f14656
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-06-21 08:37:03 +0600

    tutorial 1
    tutorial 1

---
+++ Lesson1/index.js
@@ -0,0 +1,4 @@
+const s1= require('./sutudent');
+console.log(s1.getName());
+console.log(s1.getAge());
+console.log(s1.result);
---
+++ Lesson1/sutudent.js
@@ -0,0 +1,19 @@
+const getName=()=>{
+    return "samsun naher";
+}
+
+const getAge=()=>{
+    return "25";
+}
+
+const result=2.50;
+
+// exports.getName=getName;
+// exports.getAge=getAge;
+// exports.result=result;
+
+module.exports={
+    getAge,
+    getName,
+    result,
+}
