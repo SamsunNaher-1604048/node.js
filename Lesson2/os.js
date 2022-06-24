94aef22d6cb09b45bcdd19fda82afec68dd91d2d
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-06-24 08:26:57 +0600

    day 2
    day 2

---
+++ Lesson2/os.js
@@ -0,0 +1,12 @@
+const os =require('os')
+console.log(os.userInfo())
+console.log(os.homedir())
+console.log(os.hostname())
+console.log(os.totalmem())
+console.log(os.freemem())
+//go os documentation.
+
+//to know file name and path name
+
+console.log(__dirname);
+console.log(__filename);
