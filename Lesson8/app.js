ddcac6028113efd5112be04301e6701bf4daceb5
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-06-30 19:53:37 +0600

    express routing

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -1,3 +1,15 @@
-const express=require('espress');
+const express=require('express');
 const app=express();
+
+const userroute=require('./routes/user.routes')
+app.use('/api/user/',userroute)
+
+app.get('/',(req,res)=>{
+    res.send('this is get method')
+    res.end()
+})
+app.use((req,res)=>{
+    res.send('404!!!')
+    res.end();
+})
 module.exports=app;
