3e2917b3c35d973b2f84c05eabf0bd37a874bf05
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-07-01 07:03:17 +0600

    html file, json data

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -4,9 +4,24 @@ const app=express();
 const userroute=require('./routes/user.routes')
 app.use('/api/user/',userroute)
 
+//json file
+app.use('/reg',(req,res)=>{
+    res.status(200).json({
+        name:"nishi",
+        message:"hello i am register page",
+        status:200,
+    });
+});
+
+//pass index file
+app.use('/login',(req,res)=>{
+    res.statusCode=200;
+    res.sendFile(__dirname+"/views/Login.html")
+})
+
 app.get('/',(req,res)=>{
-    res.send('this is get method')
-    res.end()
+   res.statusCode=200;
+   res.sendFile(__dirname+"/views/index.html")
 })
 app.use((req,res)=>{
     res.send('404!!!')
